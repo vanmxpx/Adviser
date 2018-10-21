fc2396d1ea257572eba5f49a402d8a3f1c8a4ff1
Misha Krygin <Noderoid64@gmail.com>
2018-10-21 10:01:34 +0300

    [chor]

--- Server/Client/src/app/Components/purchases/dynamic-chart/dynamic-chart.component.ts
+++ Server/Client/src/app/Components/purchases/dynamic-chart/dynamic-chart.component.ts
@@ -8,42 +8,35 @@ import { Chart } from 'chart.js';
   styleUrls: ['./dynamic-chart.component.scss']
 })
 export class DynamicChartComponent implements OnInit {
-
-  public Canvas = document.getElementById('canvas');
-  public myChart: Chart;
+  chart: any;
   ngOnInit() {
-    this.myChart = new Chart(this.Canvas, {
-      type: 'bar',
+    this.chart = new Chart(document.getElementById('canvas'), {
+      type: 'line',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
+        labels: ['one', 'two', 'hell', 'sss'],
+        datasets: [
+          {
+            data: [1, 2, 3, 4],
+            borderColor: '#3cba9f',
+            fill: true
+          },
+          {
+            data: [4, 3, 2, 1],
+            borderColor: '#ffcc00',
+            fill: false
+          },
+        ]
       },
       options: {
+        legend: {
+          display: false
+        },
         scales: {
+          xAxes: [{
+            display: true
+          }],
           yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
+            display: true
           }]
         }
       }
