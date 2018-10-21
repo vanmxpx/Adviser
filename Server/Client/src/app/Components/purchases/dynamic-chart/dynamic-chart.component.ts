58970f01e5c3281fcf24097b9c0cc095ee723139
Misha Krygin <Noderoid64@gmail.com>
2018-10-21 11:20:40 +0300

    [update] add charts

--- Server/Client/src/app/Components/purchases/dynamic-chart/dynamic-chart.component.ts
+++ Server/Client/src/app/Components/purchases/dynamic-chart/dynamic-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit , ElementRef, ViewChild } from '@angular/core';
 import { Chart } from 'chart.js';
 
 
@@ -7,41 +7,31 @@ import { Chart } from 'chart.js';
   templateUrl: './dynamic-chart.component.html',
   styleUrls: ['./dynamic-chart.component.scss']
 })
-export class DynamicChartComponent implements OnInit {
-  chart: any;
-  ngOnInit() {
-    this.chart = new Chart(document.getElementById('canvas'), {
-      type: 'line',
-      data: {
-        labels: ['one', 'two', 'hell', 'sss'],
-        datasets: [
-          {
-            data: [1, 2, 3, 4],
-            borderColor: '#3cba9f',
-            fill: true
-          },
-          {
-            data: [4, 3, 2, 1],
-            borderColor: '#ffcc00',
-            fill: false
-          },
-        ]
-      },
-      options: {
-        legend: {
-          display: false
-        },
-        scales: {
-          xAxes: [{
-            display: true
-          }],
-          yAxes: [{
-            display: true
-          }]
-        }
-      }
-    });
-  }
+export class DynamicChartComponent implements OnInit, AfterViewInit {
+  @ViewChild('canvas') canvas: ElementRef;
+  chart = [];
+    labels: any = ['aaa'];
 
+    data = [];
+
+    constructor() { }
+
+    ngOnInit() {
+this.data.push(12);
+    }
+    ngAfterViewInit() {
+        this.chart = new Chart(this.canvas.nativeElement.getContext('2d'), {
+            type: 'bar',
+            labels: this.labels,
+            data: {
+                labels: this.labels,
+                data: this.data
+            },
+            options: {
+                responsive: true
+            }
+        });
+
+    }
 
 }
