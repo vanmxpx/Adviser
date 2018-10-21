ea6badf1ceb9dc2ffdf5267ea3d9811fa75f6d5f
Emma Yeroshek <emma.yeroshek@gmail.com>
2018-10-21 17:19:18 +0300

    dezignedFoodCost

--- Server/Client/src/app/Components/foodcost/foodcost.component.ts
+++ Server/Client/src/app/Components/foodcost/foodcost.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import { Ingredient } from 'src/app/Models/ingredient';
+import { Product } from 'src/app/Models/product';
+
 
 
 @Component({
@@ -10,16 +12,15 @@ import { Ingredient } from 'src/app/Models/ingredient';
 })
 
 export class FoodcostComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'cost', 'supplier', 'left_overs'];
+  displayedColumns: string[] = ['position', 'name', 'cost','markup','foodcost', 'supplier', 'left_overs'];
   dataSource = ELEMENT_DATA;
   constructor() { }
   ngOnInit() {  
   }
 }
 
-
 const ELEMENT_DATA: Ingredient[] = [
-  {position: 1, name: 'Соль', cost: 400, supplier: 'Ваня Иванов', left_overs: 3},
-  {position: 2, name: 'Текила', cost: 5000, supplier: 'Jonh Doe', left_overs: 5 },
-  {position: 3, name: 'Лайм', cost: 2000, supplier: 'Fuck Fuck', left_overs: 10},
+  {position: 1, name: 'Соль', cost: 400, markup:100, foodcost: 30, supplier: 'Ваня Иванов', left_overs: 3},
+  {position: 2, name: 'Текила', cost: 5000,  markup:200,foodcost: 50,  supplier: 'Jonh Doe', left_overs: 5 },
+  {position: 3, name: 'Лайм', cost: 2000,  markup:300,foodcost: 40,  supplier: 'Fuck Fuck', left_overs: 10},
 ];
--- Server/Client/src/app/Models/ingredient.ts
+++ Server/Client/src/app/Models/ingredient.ts
@@ -2,6 +2,8 @@ export class Ingredient {
     position: number;
     name: string;
     cost: number;
+    markup: number;
+    foodcost: number;
     supplier: string;
     left_overs: number;
 }
